3160aefccbde04a3fb17c8eddb2633d22c89ca0a
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-01-10 12:43:27 -0600

    Wrong directory

---
+++ javascript-algorithms-and-data-structures/challenges/recursion-range-of-numbers.js
@@ -0,0 +1,11 @@
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum <= startNum) {
+      return [startNum];
+    } else {
+      const arr = rangeOfNumbers(startNum, endNum - 1);
+      arr.push(endNum);
+      return arr;
+    }
+  }
+  
+  console.log(rangeOfNumbers(5, 10));
